8c18a69e81dcc197ca4606ad3b60f213a6bf3073
Roman Sergeev <sergeev.rs08@gmail.com>
2022-09-17 19:02:37 +0300

    game form

--- src/main/java/com/lolanalyzer/parcer/controller/GameController.java
+++ src/main/java/com/lolanalyzer/parcer/controller/GameController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @Slf4j
 @RequestMapping("/game")
 public class GameController {
