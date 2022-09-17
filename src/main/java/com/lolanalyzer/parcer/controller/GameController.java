746f11c871bbfeb02a8fb1234d13c3fd2cbbb7db
Roman Sergeev <sergeev.rs08@gmail.com>
2022-09-17 19:28:04 +0300

    game form

--- src/main/java/com/lolanalyzer/parcer/controller/GameController.java
+++ src/main/java/com/lolanalyzer/parcer/controller/GameController.java
@@ -3,6 +3,7 @@ package com.lolanalyzer.parcer.controller;
 import com.lolanalyzer.parcer.entity.Game;
 import com.lolanalyzer.parcer.repositiory.GameRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +17,11 @@ import java.util.ArrayList;
 public class GameController {
 
     private GameRepository gameRepository;
+
+    @Autowired
+    public GameController(GameRepository gameRepository){
+        this.gameRepository = gameRepository;
+    }
     @GetMapping
     public String gameForm(Model model){
         ArrayList<Game> games = (ArrayList<Game>) gameRepository.findAll();
